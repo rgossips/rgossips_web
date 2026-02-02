0170550e6eba935e80f011eb0b7b2919630a2018
Hridey <hridey79@gmail.com>
2026-02-02 17:27:23 +0530

    build fix

--- src/components/StayCarousel.jsx
+++ src/components/StayCarousel.jsx
@@ -6,7 +6,7 @@ import { FaChevronLeft, FaChevronRight, FaInstagram } from "react-icons/fa";
 import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 import SectionTitle from "./SectionTitle";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 const fetchStays = async () => {
