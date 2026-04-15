c9fca427fa16eb26737e7416bb07a2340082cc55
Deep13 <deep.deepak30@gmail.com>
2026-04-15 17:31:50 +0530

    fix: match Instagram OAuth scopes and add force_reauth to match Meta app config

--- src/components/login/InstagramConnect.jsx
+++ src/components/login/InstagramConnect.jsx
@@ -88,9 +88,9 @@ const InstagramConnect = ({ onNext, mode = "signup", role = "influencer", loadin
   const handleConnect = () => {
     const appId = process.env.NEXT_PUBLIC_INSTAGRAM_APP_ID;
     const redirectUri = `${window.location.origin}/instagram-callback`;
-    const scope = "instagram_business_basic,instagram_business_manage_insights";
+    const scope = "instagram_business_basic,instagram_business_manage_messages,instagram_business_manage_comments,instagram_business_content_publish,instagram_business_manage_insights";
 
-    const authUrl = `https://www.instagram.com/oauth/authorize?client_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}&response_type=code`;
+    const authUrl = `https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}&response_type=code`;
 
     // Save state so we can restore after redirect
     localStorage.setItem("instagram_oauth_mode", mode);
